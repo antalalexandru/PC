4d8054fcac772fe3f7fd4f02b316f59c3280b7ee
Alexandru <crgroot97@gmail.com>
2018-11-22 14:46:31 +0200

    Login / index basic

--- src/PC/urls.py
+++ src/PC/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from voluntariat import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('admin/', admin.site.urls), # ??
+    path('login/', views.login)
 ]
--- src/voluntariat/views.py
+++ src/voluntariat/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
+
+def login(request):
+    page_content = loader.get_template('login.html').render()
+    template = loader.get_template('index.html')
+    context = {
+        'page_content': page_content,
+    }
+    return HttpResponse(template.render(context, request))
+
+def index(request):
+    page_content = "Pagina principala"
+    template = loader.get_template('index.html')
+    context = {
+        'page_content': page_content,
+    }
+    return HttpResponse(template.render(context, request))
+
