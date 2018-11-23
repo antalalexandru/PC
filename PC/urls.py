99f0b54b3d56ef2e4564ffcb9fb45b120d64d5a7
Denis <denisbarnutiu@yahoo.com>
2018-11-23 09:31:31 +0200

    events add create edit delte

--- PC/urls.py
+++ PC/urls.py
@@ -1,3 +1,5 @@
+
+
 """PC URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,8 +16,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import  settings
 urlpatterns = [
+    path('event/',include('voluntariat.urls')),
     path('admin/', admin.site.urls),
-]
+
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
--- voluntariat/admin.py
+++ voluntariat/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
 
-from voluntariat.models import User
+from voluntariat.models import User,Event
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Event)
+
