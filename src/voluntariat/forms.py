1cc5ee6d2acf13871e4daeb5d33156866683312a
Dan <ailenei_d@yahoo.com>
2018-12-09 15:44:51 +0200

    forms back

---
+++ src/voluntariat/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from voluntariat.models import User, Event
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='username', max_length=250)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+
+
+class EventForm(forms.ModelForm):
+    start_date = forms.DateTimeField(widget=forms.TextInput(attrs={"placeholder": "yyyy-mm-dd hh:mm"}))
+    end_date = forms.DateTimeField(widget=forms.TextInput(attrs={"placeholder": "yyyy-mm-dd hh:mm"}))
+
+    class Meta:
+        model = Event
+        fields = ('name', 'picture', 'location', 'description', 'benefits', 'start_date', 'end_date')
